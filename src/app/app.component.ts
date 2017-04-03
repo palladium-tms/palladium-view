c73801cbfc4ec7820f8bb38bd209d2c2fc145ea7
Dmitriy Rotatyy <flamine@list.ru>
2017-04-03 23:02:20 +0300

    add main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import {PalladiumApiService} from './servises/palladium-api.service';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-products',
+  template: `<ul>
+    {{products}}
+    <li *ngFor="let product of products">
+      <p>Имя пользователя: {{product}}</p>
+      <p>Возраст пользователя: {{product}}</p>
+    </li>
+  </ul>
+  <pre id="custom-spacing">{{ heroes | json:4 }}</pre>
+  `,
+  providers: [PalladiumApiService],
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class AppComponent implements OnInit {
+  // title = 'app works!';
+  heroes: {};
+  errorMessage;
+  constructor(private httpService: PalladiumApiService) {}
+
+  ngOnInit() {
+    this.httpService.getData('/products')
+      .subscribe(
+        heroes => {
+          this.heroes = heroes;
+        },
+        error =>  this.errorMessage = <any>error);
+  }
 }
---
+++ src/app/servises/palladium-api.service.ts
@@ -0,0 +1,39 @@
+import {Injectable} from '@angular/core';
+import {AppSettings} from '../appSettings';
+import {Http, Response} from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+
+
+@Injectable()
+
+export class PalladiumApiService {
+
+  constructor(private http: Http) {}
+  getData(path) {
+    return this.http.get(AppSettings.API_ENDPOINT + path)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    const body = JSON.parse(res['_body']);
+    return body || { };
+  }
+
+  private handleError (error: Response | any) {
+    // In a real world app, you might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
+}
