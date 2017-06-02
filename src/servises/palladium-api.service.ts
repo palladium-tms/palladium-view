e9c93d8e45e75d2cdceca1c07f54e0980df61354
flaminestone <flamine@list.ru>
2017-06-02 17:27:34 +0300

    Delete unused variable and reformat code

--- src/servises/palladium-api.service.ts
+++ src/servises/palladium-api.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {AppSettings} from '../appSettings';
 import {Headers, Http, Response, RequestOptions} from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
@@ -11,30 +11,33 @@ import 'rxjs/add/operator/toPromise';
 
 export class PalladiumApiService {
 
-  constructor(private http: Http, private RequestOptions: RequestOptions) {}
+  constructor(private http: Http) {}
 
   getData(path) {
     // console.log(JSON.parse(localStorage.getItem('auth_data'))['token']);
-    const headers = new Headers({ 'Authorization': JSON.parse(localStorage.getItem('auth_data'))['token']});
+    const headers = new Headers({'Authorization': JSON.parse(localStorage.getItem('auth_data'))['token']});
     return this.http.get(AppSettings.API_ENDPOINT + path, {headers: headers})
       .map(this.extractData)
       .catch(this.handleError);
   }
+
   postData(path, params) {
-    const headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      'Authorization': JSON.parse(localStorage.getItem('auth_data'))['token']});
-    const options = new RequestOptions({ headers: headers });
-    return this.http.post(AppSettings.API_ENDPOINT + path, params , options)
+    const headers = new Headers({
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      'Authorization': JSON.parse(localStorage.getItem('auth_data'))['token']
+    });
+    const options = new RequestOptions({headers: headers});
+    return this.http.post(AppSettings.API_ENDPOINT + path, params, options)
       .map(this.extractData)
       .catch(this.handleError);
   }
 
   private extractData(res: Response) {
     const body = JSON.parse(res['_body']);
-    return body || { };
+    return body || {};
   }
 
-  private handleError (error: Response | any) {
+  private handleError(error: Response | any) {
     // In a real world app, you might use a remote logging infrastructure
     let errMsg: string;
     if (error instanceof Response) {
