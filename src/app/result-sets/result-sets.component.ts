0341aff316838020b23e8beabbdcafd91d2c3406
flaminestone <flamine@list.ru>
2017-07-07 16:50:50 +0300

    Add   opportunity to set result

--- src/app/result-sets/result-sets.component.ts
+++ src/app/result-sets/result-sets.component.ts
@@ -19,8 +19,10 @@ export class ResultSetsComponent implements OnInit {
   errorMessage;
   result_set_settings_data = {};
   statuses;
+  statuses_array = [];
   statistic = {};
-
+  selected_counter = [];
+  selected_color;
   constructor(private activatedRoute: ActivatedRoute, private httpService: HttpService,
               private ApiService: PalladiumApiService,  private router: Router ) { }
 
@@ -29,7 +31,10 @@ export class ResultSetsComponent implements OnInit {
       this.result_sets = [];
       this.run_id = params.id;
       this.get_result_sets(this.run_id);
-      this.ApiService.get_statuses().subscribe(res => this.statuses = res);
+      this.ApiService.get_statuses().subscribe(res => {
+        this.statuses = res;
+        this.statuses_array = Object.keys(this.statuses);
+      });
     });
     if ( this.router.url.indexOf('/result_set/') >= 0) {
       $('.product-space').removeClass('very-big-column big-column').addClass('small-column');
@@ -51,7 +56,6 @@ export class ResultSetsComponent implements OnInit {
         },
         error =>  this.errorMessage = <any>error);
   }
-
   delete_result_set(modal) {
     if (confirm('A u shuare?')) {
       this.httpService.postData('/api/result_set_delete', 'result_set_data[id]=' + this.result_set_settings_data['id'])
@@ -66,7 +70,6 @@ export class ResultSetsComponent implements OnInit {
     modal.close();
     }
   }
-
   edit_result_set(form: NgForm, modal, valid: boolean) {
     if ( !valid ) { return; }
     const params = 'result_set_data[result_set_name]=' + form.value['result_set_name']
@@ -78,7 +81,6 @@ export class ResultSetsComponent implements OnInit {
             this.result_sets[this.result_set_settings_data['index']].name = result_sets.result_set_data.name;
             this.result_sets[this.result_set_settings_data['index']].updated_at = result_sets.result_set_data.updated_at;
           } else {
-            console.log(result_sets.errors);
           }
         },
         error =>  this.errorMessage = <any>error);
@@ -107,8 +109,8 @@ export class ResultSetsComponent implements OnInit {
       return {'background': this.statuses[id].color };
     }
   }
-
   calculate_statistic_of_run(data): void { // FIXME: it must be sended from run component, buti dont know how
+    this.statistic = {};
     for (const result_set of data ) {
       if (result_set['status'] in this.statistic) {
         this.statistic[result_set['status']] += 1;
@@ -118,4 +120,75 @@ export class ResultSetsComponent implements OnInit {
     }
     this.statistic['all'] = Object.keys(this.statistic);
 }
+  selectCounter(result_set) {
+    if (this.selected_counter.indexOf(result_set.id) === -1) {
+      this.selected_counter.push(result_set.id);
+    }else {
+      const index =  this.selected_counter.indexOf(result_set.id);
+      this.selected_counter.splice(index, 1);
+    }
+    this.add_result_button(this.selected_counter.length === 0);
+  }
+  add_result_button(disable: boolean) {
+    if (disable) {
+      $('.add-result-block').addClass('disabled');
+    }else {
+      $('.add-result-block').removeClass('disabled');
+    }
+  }
+  add_result_modal(modal) {
+    if (this.selected_counter.length !== 0 ) {
+      modal.open();
+      if (this.selected_counter.length === 1 ) {
+        this.set_sets_status_as_default();
+      }else {
+        this.set_first_status_as_default();
+      }
+    }
+  }
+  set_sets_status_as_default() {
+    var current_result_set;
+    $('input[type=checkbox]:checked').val();
+    this.result_sets.forEach((current_result_set) => { // FIXME: need to get result_sets like a hash. Need server side changes
+      if (+$('input[type=checkbox]:checked').val() === current_result_set['id']) {
+        this.selected_color = +this.statuses[current_result_set['status']]['id'];
+        this.header_background_change(this.statuses[current_result_set['status']]['color']);
+      }
+    });
+  }
+  set_first_status_as_default() {
+    this.selected_color = +this.statuses[this.statuses_array[0]]['id'];
+    this.header_background_change(this.statuses[this.statuses_array[0]]['color']);
+  }
+  add_results(form: NgForm, modal) {
+    const params = this.set_params_for_add_result({description: form.value['result_description'],
+      status: this.statuses[this.statuses_array[form.value['result_status'] - 1]], result_sets: this.selected_counter});
+    this.httpService.postData('/api/result_new', params)
+      .subscribe(
+        result_sets => {
+          this.result_sets.forEach((current_result_set, index) => {
+            if (result_sets['result_set_id'].includes(current_result_set['id'])) {
+              this.result_sets[index]['status'] = this.statuses_array[form.value['result_status'] - 1];
+              this.calculate_statistic_of_run(this.result_sets);
+            }
+          });
+        },
+        error =>  this.errorMessage = <any>error);
+    modal.close();
+  }
+  set_params_for_add_result(data) {
+    var params = '';
+    for (const result_set of data['result_sets']) {
+      params += 'result_data[result_set_id][]=' + result_set + '&';
+    }
+    params += 'result_data[message]=' + data['description'] + '&';
+    params += 'result_data[status]=' + data['status']['name'];
+    return(params);
+  }
+  changer_header_color() {
+    this.header_background_change(this.statuses[$('option:selected').val()]['color']);
+  }
+  header_background_change(color) {
+    $('.status_color').css('background-color', color);
+  }
 }
