06f19029e1b1f53b08d52595ea58e431daea03a1
flaminestone <kvazilife@gmail.com>
2017-11-12 20:07:58 +0300

    Add editint to case: part 1

--- src/app/cases/cases.component.ts
+++ src/app/cases/cases.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute, Params, Router} from '@angular/router';
 import {HttpService} from '../../services/http-request.service';
 import {PalladiumApiService} from '../../services/palladium-api.service';
@@ -14,11 +14,18 @@ declare var $: any;
 })
 export class CasesComponent implements OnInit {
   cases: Case[] = [];
+  @ViewChild('form') form;
+  @ViewChild('Modal') Modal;
   selected_counter = [];
   statuses;
+  object;
   statuses_array;
   selected_status_id: number;
   case_settings_data = {};
+  menuItems = [
+    {label: '<span class="menu-icon">Refresh</span>', onClick: this.get_cases.bind(this)},
+    {label: '<span class="menu-icon">Edit</span>', onClick: this.open_modal.bind(this)},
+    {label: '<span class="menu-icon">Delete</span>', onClick: this.delete_case.bind(this)}];
 
   constructor(private activatedRoute: ActivatedRoute, private httpService: HttpService,
               private ApiService: PalladiumApiService, private router: Router, private _eref: ElementRef) {
@@ -26,56 +33,35 @@ export class CasesComponent implements OnInit {
 
   ngOnInit() {
     this.activatedRoute.params.subscribe((params: Params) => {
-      this.ApiService.get_statuses().then(res => {
-        this.statuses = JSON.parse(JSON.stringify(res));
-        this.statuses_array = Object.keys(this.statuses);
-        this.statuses['0'] = {name: 'Untested', color: '#ffffff', id: 0}; // add untested status. FIXME: need to added automaticly
-      });
-      this.get_cases(params.id);
+      this.get_cases();
     });
   }
-  get_cases(id) {
+  get_cases() {
+    const id = this.router.url.match(/suite\/(\d+)/i)[1];
     this.ApiService.get_cases(id).then(cases => {
       this.cases = cases;
     });
   }
-  selectCounter(current_case) {
-    if (this.selected_counter.indexOf(current_case.id) === -1) {
-      this.selected_counter.push(current_case.id);
-    } else {
-      const index = this.selected_counter.indexOf(current_case.id);
-      this.selected_counter.splice(index, 1);
-    }
-    this.add_result_button(this.selected_counter.length === 0);
-  }
 
-  add_result_button(disable: boolean) {
-    if (disable) {
-      $('.add-result-block').addClass('disabled');
-    } else {
-      $('.add-result-block').removeClass('disabled');
-    }
-  }
-
-  show_settings_button(index) {
-    $('#' + index + '.case-setting-button').show();
-  };
-
-  hide_settings_button(index) {
-    $('#' + index + '.case-setting-button').hide();
+  open_modal(object) {
+    this.object = this.cases.filter(obj => obj.id === object.dataContext.id)[0];
+    this.form.controls['name'].setValue(this.object.name);
+    this.Modal.open();
   };
 
   edit_case(form: NgForm, modal, valid: boolean) {
     if (!valid) {
       return;
     }
-    this.ApiService.edit_case(this.case_settings_data['id'], form.value['case_name']).then(
+    console.log(form);
+    this.ApiService.edit_case(this.object.id, form.value['name']).then(
       this_case => {
-          this.cases[this.case_settings_data['index']].name = this_case.name;
+        const result_set_for_change = this.result_sets.filter(result_set => result_set.id === this.object.id)[0];
+        this.cases[this.case_settings_data['index']].name = this_case.name;
           this.cases[this.case_settings_data['index']].updated_at = this_case.updated_at;
         }
     );
-    modal.close();
+    this.Modal.close();
   }
 
   delete_case(casesettingsModal) {
@@ -86,6 +72,7 @@ export class CasesComponent implements OnInit {
       casesettingsModal.close();
     }
   }
+
   settings(modal, this_case, index, form) {
     this.case_settings_data = {id: this_case.id, index: index};
     modal.open();
