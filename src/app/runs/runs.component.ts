3425fe58d3d396027facfaa04d1ae64357b386bd
flaminestone <kvazilife@gmail.com>
2017-11-20 02:36:01 +0300

    Add buttons to runs and update all for angular 5

--- src/app/runs/runs.component.ts
+++ src/app/runs/runs.component.ts
@@ -28,10 +28,6 @@ export class RunsComponent implements OnInit {
   statuses;
   object;
   statistic: Statistic;
-  menuItems = [
-    {label: '<span class="menu-icon">Refresh</span>', onClick: this.get_runs_and_suites.bind(this)},
-    {label: '<span class="menu-icon">Edit</span>', onClick: this.open_modal.bind(this)},
-    {label: '<span class="menu-icon">Delete</span>', onClick: this.delete_object_modal.bind(this)}];
   filter = [];
 
   constructor(private ApiService: PalladiumApiService, private activatedRoute: ActivatedRoute,
@@ -94,6 +90,7 @@ export class RunsComponent implements OnInit {
   }
 
   get_runs_and_suites() {
+    this.runs_and_suites = [];
     Promise.all([this.get_runs(this.plan_id), this.get_suites(), this.get_statuses()]).then(res => {
       this.statuses = res[2];
       this.suites = res[1];
@@ -146,54 +143,74 @@ export class RunsComponent implements OnInit {
   }
 
   edit_object_modal(form: NgForm, modal) {
-    if (this.object.path === './run') {
-      this.ApiService.edit_suite_by_run_id(this.object.id, form.value['name']).then(suite => {
-        this.runs_and_suites.forEach(object => {
-          if (object.id === this.object.id && object.path === './run') {
-            object.name = suite.name;
-            object.updated_at = suite.updated_at;
-          }
-        }); // FIXME: need optimize
+    if (this.run_opened()) {
+      this.ApiService.edit_suite_by_run_id(this.object.id, form.value['name']).then((suite: Suite) => {
+        const edited = this.runs_and_suites[this.runs_and_suites.indexOf(this.runs_and_suites.filter(it => it.id === this.object.id)[0])];
+        edited.name = suite.name;
+        edited.updated_at = suite.updated_at;
       });
     } else {
-      this.ApiService.edit_suite(this.object.id, form.value['name']).then(suite => {
-        this.runs_and_suites.forEach(object => {
-          if (object.id === this.object.id && object.path === './suite') {
-            object.name = suite.name;
-            object.updated_at = suite.updated_at;
-          }
-        }); // FIXME: need optimize
+      this.ApiService.edit_suite(this.object.id, form.value['name']).then((suite: Suite)  => {
+        const edited = this.runs_and_suites[this.runs_and_suites.indexOf(this.runs_and_suites.filter(it => it.id === this.object.id)[0])];
+        edited.name = suite.name;
+        edited.updated_at = suite.updated_at;
       });
     }
     modal.close();
   }
-  delete_object_modal(object) {
+  delete_object() {
     if (confirm('A u shuare?')) {
-      if (object.dataContext.path === './run') {
-        this.ApiService.delete_run(object.dataContext.id).then(run => {
+      const id = this.get_items_id();
+      if (this.run_opened()) {
+        this.ApiService.delete_run(id).then(run => {
           this.runs = this.runs.filter(current_run => current_run.id !== +run);
           this.merge_suites_and_runs();
           this.statistic = this.StatisticService.runs_and_suites_statistic(this.runs_and_suites);
-          if (this.router.url.indexOf('/run/' + object.dataContext.id) >= 0) {
+          if (this.router.url.indexOf('/run/' + id) >= 0) {
             this.router.navigate([/(.*?)(?=run|$)/.exec(this.router.url)[0]]);
           }
         });
       } else {
-        this.ApiService.delete_suite(object.dataContext.id).then(suite => {
-          const deleted_obj = this.runs_and_suites.filter(obj => (obj.id === suite.id && obj.path === './suite'))[0];
-          this.runs_and_suites.splice(this.runs_and_suites.indexOf(deleted_obj), 1);
+        this.ApiService.delete_suite(id).then((suite: Suite) => {
+          this.suites = this.suites.filter(obj => (obj.id !== suite.id));
+          this.merge_suites_and_runs();
           this.statistic = this.StatisticService.runs_and_suites_statistic(this.runs_and_suites);
-          if (this.router.url.indexOf('/suite/' + object.dataContext.id) >= 0) {
+          if (this.router.url.indexOf('/suite/' + id) >= 0) {
             this.router.navigate([/(.*?)(?=suite|$)/.exec(this.router.url)[0]]);
           }
         });
       }
     }
   }
-  open_modal(object) {
-    this.object = this.runs_and_suites.filter(current_object => current_object.id === object.dataContext.id &&
-      current_object.path === object.dataContext.path)[0];
+  open_modal() {
+    this.object = this.runs_and_suites.filter(current_object => current_object.id === this.get_items_id() &&
+      current_object.path === this.opened_item())[0];
     this.form.controls['name'].setValue(this.object.name);
     this.Modal.open();
   };
+
+  toolbar_opened() {
+    return (this.run_opened() || this.suite_opened());
+  }
+  run_opened() {
+    return this.router.url.indexOf('run') >= 0;
+  }
+  suite_opened() {
+    return this.router.url.indexOf('suite') >= 0;
+  }
+  opened_item() {
+    if (this.run_opened()) {
+      return ('./run');
+    } else {
+      return ('./suite');
+    }
+  }
+
+  get_items_id() {
+    if (this.run_opened()) {
+      return ( +/run\/(\d+)/.exec(this.router.url)[1]);
+    } else {
+      return ( +/suite\/(\d+)/.exec(this.router.url)[1]);
+    }
+  }
 }
