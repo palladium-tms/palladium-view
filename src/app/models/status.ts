6c854934fa5d2d1bde32a05c865a073f7234ed85
flaminestone <kvazilife@gmail.com>
2017-11-27 18:42:39 +0300

    Change status settings. Add oppotrunity to create status

--- src/app/models/status.ts
+++ src/app/models/status.ts
@@ -1,8 +1,17 @@
 export class Status {
+  id: number;
   name: string;
   color: string;
+  active = false;
   constructor(status_data) {
-    this.name = status_data['name'];
-    this.color = status_data['color'];
+    if (status_data === null) {
+      this.id = 0;
+      this.name = '';
+      this.color = '';
+    } else {
+      this.id = status_data['id'];
+      this.name = status_data['name'];
+      this.color = status_data['color'];
+    }
   }
 }
--- src/app/top-toolbar/status-settings/status-settings.component.ts
+++ src/app/top-toolbar/status-settings/status-settings.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {PalladiumApiService} from '../../../services/palladium-api.service';
-declare var $: any;
+import {Status} from '../../models/status';
 
 @Component({
   selector: 'app-status-settings',
@@ -8,36 +8,58 @@ declare var $: any;
   styleUrls: ['./status-settings.component.css']
 })
 export class StatusSettingsComponent implements OnInit {
+  @ViewChild('form') form;
+  @ViewChild('creating_form') creating_form;
   statuses;
-  statuses_array;
+  selected;
+  mode = 'empty';
   constructor(private ApiService: PalladiumApiService) { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
   status_settings(modal) {
     modal.open();
     this.ApiService.get_not_blocked_statuses().then(res => {
       this.statuses = res;
-      this.statuses_array = Object.keys(this.statuses);
     });
   }
-  block_status(id) {
-    this.ApiService.block_status(id).then(res => {
-      this.statuses[res['status']['id'].toString()] = res;
-      if (this.statuses[res['status']['id'].toString()]['status']['block']) {
-        $('#' + id).hide();
-      }
-    });
-  }
-  change_color(id, color) {
-    this.ApiService.color_status(id, color).then(res => {
-      console.log(this.statuses[id]['color']);
-      this.statuses[id]['color'] = res['status']['color'];
-    });
-  }
-  getStylesBackround(id) {
-    if (this.statuses) {
-      return {'background': this.statuses[id].color };
+  delete_status() {
+    if (confirm('A u shuare?')) {
+      this.ApiService.block_status(this.selected.id).then(res => {
+        this.statuses = this.statuses.filter(status => status.id !== res['id']);
+        this.selected = null;
+        this.mode = 'empty';
+      });
     }
+  }
+  update_status() {
+    const name = this.form.value['status_name'];
+    const color = this.form.value['status_color'];
+    this.ApiService.update_status(this.selected.id, name, color).then(res => {
+      this.selected.name = res.name;
+      this.selected.color = res.color;
+    });
+  }
+  getStyles(object) {
+      return {'border-left': '7px solid ' + object.color};
+  }
+  status_new() {
+    const name = this.creating_form.value['status_name'];
+    const color = this.creating_form.value['status_color'];
+    this.ApiService.status_new(name, color).then( status => {
+      this.statuses.push(new Status(status));
+      this.selected = this.statuses.filter(st => st.id === status.id)[0];
+      this.item_selected(this.statuses.filter(st => st.id === status.id)[0]);
+    });
+  }
+  new_status_create() {
+    this.selected = null;
+    this.mode = 'creating';
+  }
+
+  item_selected(object) {
+    this.mode = 'editing';
+    this.selected = object;
+    this.form.controls['status_name'].setValue(object.name);
+    this.form.controls['status_color'].setValue(object.color);
   }
 }
