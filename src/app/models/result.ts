479a47a45940539d7d2e6649f94fe5f744b40ede
flaminestone <kvazilife@gmail.com>
2018-01-18 15:31:08 +0300

    Change request content type

--- src/app/models/result.ts
+++ src/app/models/result.ts
@@ -1,12 +1,37 @@
 export class Result {
   id: number;
-  message: string;
   status_id: string;
   created_at: number;
+  subdescriber = [];
+  describer;
+
   constructor(data) {
-    this.id = data['id'];
-    this.message = data['message'];
+    this.id = data.id;
+    if (this.isJson(data.message)) {
+      const message = JSON.parse(data.message);
+      if (message.subdescriber && message.describer) {
+        if (message.subdescriber instanceof Array) {
+          this.describer = message.describer;
+          this.subdescriber = message.subdescriber;
+        } else {
+          this.describer = data.message;
+        }
+      } else {
+        this.describer = JSON.parse(data.message);
+      }
+    } else {
+      this.describer = data.message;
+    }
     this.status_id = data['status_id'];
     this.created_at = data['created_at'];
+  }
+
+  isJson(str) {
+    try {
+      JSON.parse(str);
+    } catch (e) {
+      return false;
+    }
+    return true;
   }
 }
