64bb2b76f508a832e2162a167ccc68e19ad00ad5
Dmitriy Rotatyy <kvazilife@gmail.com>
2018-04-15 21:57:32 +0300

    Feature/add material design to login page (#93)
    * New login page with reactive form style and material design
    
    * New login and registration pages with reactive form style and material design

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthenticationService } from '../../services/authentication.service';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthenticationService} from '../../services/authentication.service';
+import {FormControl, Validators, FormGroup} from '@angular/forms';
 
 @Component({
   moduleId: module.id,
@@ -9,15 +10,21 @@ import { AuthenticationService } from '../../services/authentication.service';
 })
 
 export class LoginComponent implements OnInit {
+  email = new FormControl('', [Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,6}$'), Validators.required]);
+  password = new FormControl('', [Validators.required, Validators.minLength(4)]);
+  login_form = new FormGroup({
+    email: this.email,
+    password: this.password,
+  });
   model: any = {};
   loading = false;
   load_form = false;
+  hide = true;
   error = '';
 
-  constructor(
-    private router: Router,
-    private authenticationService: AuthenticationService,
-  ) { }
+  constructor(private router: Router,
+              private authenticationService: AuthenticationService) {
+  }
 
   ngOnInit() {
     // reset login status
@@ -35,7 +42,7 @@ export class LoginComponent implements OnInit {
     this.loading = true;
     this.authenticationService.login(this.model.username, this.model.password)
       .then(result => {
-          this.router.navigate(['/']);
+        this.router.navigate(['/']);
         this.loading = false;
       }, error => {
         this.error = 'Username or password is incorrect';
