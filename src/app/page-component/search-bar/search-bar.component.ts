c0f12b151b1e8045de28d37a6b0dc5953d492c44
flaminestone <flamine@list.ru>
2018-07-04 10:07:44 +0300

    Add search pipe and search par component

---
+++ src/app/page-component/search-bar/search-bar.component.ts
@@ -0,0 +1,15 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent implements OnInit {
+  @Output() text = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
