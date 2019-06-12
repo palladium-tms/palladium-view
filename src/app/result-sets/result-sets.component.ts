41838ccc647e7b9bef959a218d7c66b2b8d52981
Dmitriy Rotatyy <flamine@list.ru>
2019-06-12 18:00:35 +0300

    Fix/drop search after change result set (#159)
    * Clear search after run change
    
    * Refactoring

--- src/app/result-sets/result-sets.component.ts
+++ src/app/result-sets/result-sets.component.ts
@@ -35,7 +35,7 @@ export class ResultSetsComponent implements OnInit, OnDestroy {
   statistic: Statistic = new Statistic(null);
   cases;
   object;
-  ResultComponent;
+  resultComponent;
   statuses = [];
   notBlockedStatus = [];
   statusesFormated = {};
@@ -47,7 +47,7 @@ export class ResultSetsComponent implements OnInit, OnDestroy {
   runId;
   params;
   searchToggle: SearchToggle;
-  searchValue = '';
+  searchValue;
 
   constructor(private activatedRoute: ActivatedRoute, public stat: StatisticService,
               private palladiumApiService: PalladiumApiService, private router: Router,
@@ -60,6 +60,7 @@ export class ResultSetsComponent implements OnInit, OnDestroy {
     this.params = this.activatedRoute.url.subscribe((params: Params) => {
       this.runId = params[1].path;
       this.object = null;
+      this.searchValue = '';
       this.get_result_sets_and_cases();
     });
   }
@@ -276,13 +277,13 @@ export class ResultSetsComponent implements OnInit, OnDestroy {
   update_click() {
     this.get_result_sets_and_cases();
     this.selectAllFlag = false;
-    if (this.ResultComponent && this.router.url.match(/result_set\/(\d+)/i) !== null) {
-      this.ResultComponent.init_results();
+    if (this.resultComponent && this.router.url.match(/result_set\/(\d+)/i) !== null) {
+      this.resultComponent.init_results();
     }
   }
 
   onActivate(componentRef) {
-    this.ResultComponent = componentRef;
+    this.resultComponent = componentRef;
   }
 
   clicked(event, object) {
