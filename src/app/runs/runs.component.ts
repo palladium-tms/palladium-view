897048673c7bcd994ef63a6d71c7ce82835be91e
Dmitriy Rotatyy <flamine@list.ru>
2019-07-17 14:34:04 +0300

    Return settings for runs

--- src/app/runs/runs.component.ts
+++ src/app/runs/runs.component.ts
@@ -33,6 +33,7 @@ export class RunsComponent implements OnInit, OnDestroy {
   existed_statuses = {};
   all_statistic = {};
   selected_object: Run;
+  object_for_settings;
   public Math: Math = Math;
 
   constructor(private ApiService: PalladiumApiService, private activatedRoute: ActivatedRoute,
@@ -154,10 +155,10 @@ export class RunsComponent implements OnInit, OnDestroy {
     }
   }
 
-  open_settings(object) {
+  open_settings() {
     const dialogRef = this.dialog.open(RunsSettingsComponent, {
       data: {
-        object: object,
+        object: this.object_for_settings,
         suites: this.suites,
       }
     });
