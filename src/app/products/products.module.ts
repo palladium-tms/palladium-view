b7c3961b59b1fb381a046c1331997f006a679d92
Dmitriy Rotatyy <flamine@list.ru>
2019-07-23 16:06:24 +0300

    Delete extra modules

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -8,10 +8,23 @@ import {TokenComponent, TokenDialogComponent} from '../top-toolbar/token/token.c
 import {InviteComponent, InviteDialogComponent} from '../top-toolbar/invite/invite.component';
 import {AboutComponent, AboutDialogComponent} from '../top-toolbar/about/about.component';
 import {StatusSettingsComponent, StatusSettingsDialogComponent} from '../top-toolbar/status-settings/status-settings.component';
-import {AuthGuard} from '../_guards/auth.guard';
+import {PlansComponent, PlansSettingsComponent} from '../plans/plans.component';
+import {RunsComponent, RunsSettingsComponent} from '../runs/runs.component';
+import {ResultSetsComponent, ResultSetsSettingsComponent} from '../result-sets/result-sets.component';
+import {ResultsComponent} from 'app/results/results.component';
+import {CaseHistoryComponent} from '../case-history/case-history.component';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import {StatisticService} from '../../services/statistic.service';
 
 const productRoutes: Routes = [{ path: '', component: ProductsComponent, children: [
-    {path: 'product/:id', loadChildren: '../plans/plans.module#PlansModule', canActivate: [AuthGuard]},
+    {path: 'product/:id', component: PlansComponent, children: [
+        {path: 'plan/:id', component: RunsComponent, children: [
+            {path: 'run/:id', component: ResultSetsComponent, children: [
+              {path: 'result_set/:id', component: ResultsComponent},
+              {path: 'case_history/:id', component: CaseHistoryComponent},
+            ]}
+          ]}
+      ]},
     {path: 'settings', loadChildren: '../settings/settings.module#SettingsModule'}
   ]}];
 
@@ -22,32 +35,14 @@ const productRoutes: Routes = [{ path: '', component: ProductsComponent, childre
     InviteComponent, InviteDialogComponent,
     AboutComponent, AboutDialogComponent,
     StatusSettingsComponent,
-    StatusSettingsDialogComponent],
+    StatusSettingsDialogComponent, PlansComponent, RunsComponent, ResultSetsComponent, ResultsComponent, CaseHistoryComponent, ResultSetsSettingsComponent, PlansSettingsComponent, RunsSettingsComponent],
   imports: [
     RouterModule.forChild(productRoutes),
-    AppMaterialModule, DragDropModule
+    AppMaterialModule, DragDropModule, ScrollingModule
   ], entryComponents: [ProductSettingsComponent, TopToolbarComponent, TokenComponent, TokenDialogComponent, InviteComponent, InviteDialogComponent,
     AboutComponent,
     AboutDialogComponent,
     StatusSettingsComponent,
-    StatusSettingsDialogComponent]
+    StatusSettingsDialogComponent, ResultSetsSettingsComponent, PlansSettingsComponent, RunsSettingsComponent], providers: [StatisticService]
 })
 export class ProductsModule {}
-// , children: [
-//   {
-//     path: 'product/:id', component: PlansComponent, children: [
-//       {
-//         path: 'plan/:id', component: RunsComponent, children: [
-//           {
-//             path: 'run/:id', component: ResultSetsComponent, children: [
-//               {path: 'result_set/:id', component: ResultsComponent},
-//               {path: 'case/:id', component: CaseComponent},
-//               {path: 'case_history/:id', component: CaseHistoryComponent},
-//             ]
-//           },
-//           {path: 'suite/:id', component: CasesComponent}
-//         ]
-//       }
-//     ]
-//   }, {path: 'settings', component: SettingsComponent, canActivate: [AuthGuard]},
-// ]
