8b52a23037e596d7f9f31c497025d839f8003724
Dmitriy Rotatyy <flamine@list.ru>
2019-08-01 14:36:57 +0300

    Delete preinstalled tests and npm update

--- src/app/pipes/statistic-filter/statistic-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StatisticFilterPipe } from './statistic-filter.pipe';
-
-describe('StatisticFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new StatisticFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
