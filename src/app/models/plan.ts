f8368ce2d5a3cac4b5d166da8cd3680d3f77d435
Dmitriy Rotat <kvazilife@gmail.com>
2019-12-15 21:18:09 +0300

    Add new parameter for plan object

--- src/app/models/plan.ts
+++ src/app/models/plan.ts
@@ -2,6 +2,7 @@ export class Plan {
   id: number;
   name: string;
   product_id: string;
+  isArchived: string;
   created_at: number;
   updated_at: number;
   statistic$;
@@ -9,6 +10,7 @@ export class Plan {
       this.id = plan['id'];
       this.name = plan['name'];
       this.product_id = plan['product_id'];
+      this.isArchived = plan['is_archived'];
       this.created_at = plan['created_at'].split(' +')[0];
       this.updated_at = plan['updated_at'].split(' +')[0];
   }
