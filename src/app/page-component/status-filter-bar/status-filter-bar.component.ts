0aeea17559b9fef674517acb5c85a5ff67bc0917
Dmitriy Rotat <kvazilife@gmail.com>
2020-02-29 21:50:08 +0300

    delete unused code

--- src/app/page-component/status-filter-bar/status-filter-bar.component.ts
+++ src/app/page-component/status-filter-bar/status-filter-bar.component.ts
@@ -9,12 +9,14 @@ import {Observable} from 'rxjs';
 
 export class StatusFilterBarComponent implements OnInit, OnChanges{
   @Input() statistic$: Observable<Statistic>;
+  @Input() caseCount$: Observable<number>;
   @Output() selected = new EventEmitter<number[]>();
   pointsActivity: PointActivityInterface[] = [];
 
   constructor(private cd: ChangeDetectorRef) {}
 
   ngOnInit() {
+    this.caseCount$.map(x => console.log(x)).subscribe();
     this.statistic$.map(statistic => {
       this.pointsActivity = [];
       statistic.points.forEach(point => {
