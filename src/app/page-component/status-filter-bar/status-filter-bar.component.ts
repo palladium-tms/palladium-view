edfff4a423b4175b73be1ff1f0e7633658e46c29
Dmitriy Rotatyy <flamine@list.ru>
2020-03-10 18:07:07 +0300

    workaround for undefined caseCount$

--- src/app/page-component/status-filter-bar/status-filter-bar.component.ts
+++ src/app/page-component/status-filter-bar/status-filter-bar.component.ts
@@ -18,17 +18,19 @@ export class StatusFilterBarComponent implements OnInit, OnChanges {
   }
 
   ngOnInit() {
-    this.caseCount$.switchMap(count => {
-      return this.statistic$.map(statistic => {
-        this.pointsActivity = [];
-        let allResults = 0;
-        statistic.points.forEach(point => {
-          allResults += point.count;
-          this.pointsActivity.push({point, active: false});
+    if (this.caseCount$) {
+      this.caseCount$.switchMap(count => {
+        return this.statistic$.map(statistic => {
+          this.pointsActivity = [];
+          let allResults = 0;
+          statistic.points.forEach(point => {
+            allResults += point.count;
+            this.pointsActivity.push({point, active: false});
+          });
+          this.untestedPointActivity = {point: new Point(0, count - allResults, count), active: false};
         });
-        this.untestedPointActivity = {point: new Point(0, count - allResults, count), active: false};
-      });
-    }).subscribe();
+      }).subscribe();
+    }
   }
 
   ngOnChanges() {
