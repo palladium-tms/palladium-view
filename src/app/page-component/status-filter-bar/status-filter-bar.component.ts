ae739fce477c7346d5a8fb3e54a2fcc299be1a1d
Dmitriy Rotatyy <flamine@list.ru>
2020-03-10 18:07:50 +0300

    add untested status to argument for event emit

--- src/app/page-component/status-filter-bar/status-filter-bar.component.ts
+++ src/app/page-component/status-filter-bar/status-filter-bar.component.ts
@@ -45,6 +45,9 @@ export class StatusFilterBarComponent implements OnInit, OnChanges {
   select_filter(point: PointActivityInterface): void {
     point.active = !point.active;
     const _filter = this.pointsActivity.filter(key => key.active).map(pointActivity => pointActivity.point.status);
+    if (this.untestedPointActivity.active) {
+      _filter.push(this.untestedPointActivity.point.status);
+    }
     this.selected.emit(_filter);
     this.cd.detectChanges();
   }
