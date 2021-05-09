ee4a9dc7cff4fd97ff1345ba480c94a2d783eb0f
Dmitriy Rotat <kvazilife@gmail.com>
2021-05-09 23:22:18 +0300

    fix: erros if plans not exist in product

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -19,10 +19,13 @@ export class Product {
       this.name = product['name'];
       this.createdAt = product['created_at'].split(' +')[0];
       this.updatedAt = product['updated_at'].split(' +')[0];
-      this.last_plan = {
-        name: product['last_plan']['name'],
-        id: product['last_plan']['id'],
-        updated_at: new Date(product['last_plan']['updated_at'])
-      };
+      if (product['last_plan']) {
+        this.last_plan = {
+          name: product['last_plan']['name'],
+          id: product['last_plan']['id'],
+          updated_at: new Date(product['last_plan']['updated_at'])
+        };
+      }
+
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  host: 'http://0.0.0.0:9292'
+  host: 'http://palladium.teamlab.info'
 };
