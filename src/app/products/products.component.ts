bef1a08531c6cea8882b358a8c2312ff268c26e8
Dmitriy Rotat <kvazilife@gmail.com>
2021-05-30 16:37:32 +0300

    use scss instead of css

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -17,8 +17,8 @@ import { InstantErrorStateMatcher } from 'app/validates_and_matchers/instant-err
 @Component({
   selector: 'app-products',
   templateUrl: 'products.component.html',
-  styleUrls: ['products.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['products.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
 export class ProductsComponent implements OnInit, OnDestroy {
