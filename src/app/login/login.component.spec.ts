452991169ea1475f68d980291c4e567fd63eaf89
MaxSorokin <askonev007@gmail.com>
2024-07-24 19:46:57 +0300

    fix: import Mat* & BrowserAnimations dependencies

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -4,6 +4,10 @@ import { Router } from '@angular/router';
 import { AuthenticationService } from '../../services/authentication.service';
 import { ChangeDetectorRef } from '@angular/core';
 import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card'; // Import MatCardModule
+import { MatFormFieldModule } from '@angular/material/form-field'; // Import MatFormFieldModule
+import { MatInputModule } from '@angular/material/input'; // Import MatInputModule
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Import BrowserAnimationsModule
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -19,14 +23,20 @@ describe('LoginComponent', () => {
 
     await TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
-      imports: [ ReactiveFormsModule ],
+      imports: [
+        ReactiveFormsModule,
+        MatCardModule, // Add MatCardModule to imports
+        MatFormFieldModule, // Add MatFormFieldModule to imports
+        MatInputModule, // Add MatInputModule to imports
+        BrowserAnimationsModule // Add BrowserAnimationsModule for Angular Material animations
+      ],
       providers: [
         { provide: Router, useValue: routerSpy },
         { provide: AuthenticationService, useValue: authServiceSpy },
         { provide: ChangeDetectorRef, useValue: cdSpy }
       ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
